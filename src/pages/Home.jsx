ddcbd2c13b295ba889f0110064f640ea5179f403
JoanFV <joanfernandezvilana@gmail.com>
2024-11-16 19:21:01 +0100

    Added Alert

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import UserResults from "../components/users/UserResults"
 import UserSearch from "../components/users/UserSearch"
+import Alert from '../components/layout/Alert'
 
 function Home() {
   return (
     <div>
+      <Alert />
       <UserSearch />
         <UserResults />
     </div>
